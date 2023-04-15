4f57a8b703b8ea4594dfd12c30210e809ea27433
Peter Key <petekey@duck.com>
2023-04-15 13:57:52 +1000

    Step 9 - Give Header username attribute

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,10 @@ export function Card(props) {
     return (
         <div>
             Hello
-            <Header profileImg={props.commentObject.profileImg}/>
+            <Header 
+                profileImg={props.commentObject.profileImg}
+                username={props.commentObject.username}
+            />
             <Body />
         </div>
     )
